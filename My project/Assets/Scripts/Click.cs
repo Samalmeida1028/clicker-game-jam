99f044641f21b5c920fdfad109e3189890facecc
Ethan Ferrabelo <74121414+eferrabelo1114@users.noreply.github.com>
2022-05-31 16:05:14 -0600

    ROPE

---
+++ My project/Assets/Scripts/Click.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public Camera Camera;
+    
+    private bool HasFish = false;
+
+    private bool IsFish(RaycastHit2D hitObject) {
+        // Check if the object hit is a fish
+        if (hitObject.transform == null) { return false; }
+        if (hitObject.transform.GetComponent<FishBehavior>() == null) { return false; }
+        if (hitObject.transform.name != "Fish") { return false; }
+        
+        return true;
+    }
+
+    void Update() {   
+        // Begin checking for mouse clicks on fishies
+        if(Input.GetMouseButtonDown(0) && !HasFish) {
+            Ray ray = Camera.ScreenPointToRay( Input.mousePosition ); // Create a ray from the camera to the mouse pos
+            RaycastHit2D hit = Physics2D.Raycast( ray.origin, ray.direction, Mathf.Infinity ); // Look at this later but casts the ray
+            
+            if (IsFish(hit)) {
+                // We hit a fish
+                Transform fish = hit.transform;
+                fish.GetComponent<FishBehavior>().Catch();
+                HasFish = true;
+            }
+        }
+    }
+}
---
+++ My project/Assets/Scripts/FishScriptableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FishScriptableObject", menuName = "ScriptableObjects/Fish")]
+public class FishScriptableObject : ScriptableObject
+{
+    public float weight = 0.0f;
+}
