45cfe24a19ce94a21eb4a52433586251fbe9188a
Ethan Ferrabelo <74121414+eferrabelo1114@users.noreply.github.com>
2022-06-01 15:28:04 -0600

    Update FishBehavior.cs

--- My project/Assets/Scripts/FishBehavior.cs
+++ My project/Assets/Scripts/FishBehavior.cs
@@ -4,66 +4,74 @@ using UnityEngine;
 
 public class FishBehavior : MonoBehaviour
 {
-    public int interpolationFramesCount = 2; // Number of frames to completely interpolate between the 2 positions
-    private int elapsedFrames = 0;
-
     private Vector3 targetPos;
-    private Vector3 beingPulledTowards;
-    private Vector3 beingPulledFrom;
+    private Vector3 currentGoalPosition;
+    private Vector3 velocity;
+    private Vector3 lastPos;
 
-    private float speed = 13.0F;
-
-    // Time when the movement started.
-    private float startTime;
-
-    // Total distance between the markers.
-    private float journeyLength;
+    private float maxSpeed = 4.0f;
     
+
     [SerializeField]
     public FishScriptableObject fishType;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject FishingLine = GameObject.Find("Weight");
+        if (FishingLine != null) {
+            this.targetPos = FishingLine.transform.position;
+            Debug.Log("Found");
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (!this.GetComponent<FlockAgent>().isClicked) { return; }
-        if (this.beingPulledTowards == new Vector3(0, 0, 0) || this.beingPulledFrom == new Vector3(0, 0, 0)) { return; }
 
-        // Distance moved equals elapsed time times speed..
-        float distCovered = (Time.time - startTime) * speed;
-
-        // Fraction of journey completed equals current distance divided by total distance.
-        float fractionOfJourney = distCovered / journeyLength;
-
-        // Set our position as a fraction of the distance between the markers.
-        this.transform.position = Vector3.Lerp(beingPulledFrom, beingPulledTowards, fractionOfJourney);
+        // Get the direction towards the getting caught area
+        Vector3 dirTowardsRod = (this.targetPos - this.transform.position).normalized;
         
-        float dist = (this.transform.position - this.targetPos).magnitude;
-        if (dist <= 1) {
+        //
+        transform.position += velocity * Time.deltaTime;
+
+        // If the fish has made it to the rod location(Where it is caught) catch it
+        if ((this.targetPos - this.transform.position).magnitude < 1) {
             this.Catch();
+            return;
+        }
+
+        // If the fish is moving towards a goal position, check when they reach that position then reset their velocity to fight
+        if (currentGoalPosition != new Vector3(0, 0, 0)) {
+            float magnitudeTowardsGoal = (this.currentGoalPosition - this.transform.position).magnitude;
+
+            if (magnitudeTowardsGoal <= 0.5) {
+                velocity -= (4 * dirTowardsRod) * Time.deltaTime; 
+            }
+
+            if (magnitudeTowardsGoal <= 0.1) { // If the fish gets to its goal target begin moving away again
+                //velocity = new Vector3(0, 0, 0);
+                currentGoalPosition = new Vector3(0, 0, 0);
+                return;
+            }
+        } else {
+            if (velocity.magnitude >= maxSpeed) { return; }
+            
+            velocity += (10 * -dirTowardsRod) * Time.deltaTime;
         }
     }
 
     public void Pulled(Vector3 pulledTowardsPos, float amount) {
-        this.targetPos = pulledTowardsPos;
         Vector3 a = this.transform.position;
         Vector3 b = (pulledTowardsPos - a).normalized;
 
         Vector3 pulledTowards = a + (amount * b);
 
-        this.beingPulledTowards = pulledTowards;
-        this.beingPulledFrom = a;
+        Vector3 dirTowardsGoal = (pulledTowards - this.transform.position).normalized;
 
-        // Keep a note of the time the movement started.
-        this.startTime = Time.time;
-
-        // Calculate the journey length.
-        this.journeyLength = Vector3.Distance(this.beingPulledFrom, this.beingPulledTowards);
+        this.currentGoalPosition = pulledTowards;
+        this.velocity = dirTowardsGoal * 2.0f; // Rod Pull Amount with force
+        this.lastPos = this.transform.position;
     }   
 
 
