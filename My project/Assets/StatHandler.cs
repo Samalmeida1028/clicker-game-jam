092c7eee169bb8ec9105362532a90a46a5623a16
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2022-06-03 16:26:51 -0400

    Changing up stat handlere

--- My project/Assets/StatHandler.cs
+++ My project/Assets/StatHandler.cs
@@ -10,15 +10,4 @@ public class StatHandler : MonoBehaviour
     int totalFishHooked;
     int totalFishCaught;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
