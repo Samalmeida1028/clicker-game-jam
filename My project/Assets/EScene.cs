5a9178957d3c72d42a2337895e04bd83ee714501
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2022-06-05 11:47:53 -0400

    Adding logic to switch scenes

---
+++ My project/Assets/EScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EScene : MonoBehaviour
+{
+    
+    public void LIslandScen(){
+        SceneManager.LoadScene("IslandScreen");
+    }
+}
