3ba954ddfbf022de4f9a17369961217fa4102899
ChilliCreek <cerdenebileg@umass.edu>
2022-06-05 22:17:27 -0400

    Screw Samwise

--- My project/Assets/PlayerOnPier.cs
+++ My project/Assets/PlayerOnPier.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerOnPier : MonoBehaviour
 {
+    private GameObject fishingButton;
+    private Image image;
+    private Button button;
+    private TMP_Text text;
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        fishingButton = GameObject.Find("FishingButton");
+        image = fishingButton.GetComponent<Image>();
+        button = fishingButton.GetComponent<Button>();
+        text = fishingButton.GetComponentInChildren<TMP_Text>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("player entered pier");
+        image.enabled = true;
+
+        button.enabled = true;
+
+        text.enabled = true;
     }
 
     // Update is called once per frame
     void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("player entered pier");
+        Debug.Log("player exited pier");
+        image.enabled = false;
+
+        button.enabled = false;
+
+        text.enabled = false;
     }
 }
